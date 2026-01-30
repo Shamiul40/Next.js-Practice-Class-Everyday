d135b54b7f47b07fce3267f7b2fc959375600283
Shamiul40 <shamiul4040@gmail.com>
2026-01-30 19:52:32 +0600

    parallel routing end

--- Next.js Class/module-8-advance-routing/app/parallel-dashbord/@notifications/page.js
+++ Next.js Class/module-8-advance-routing/app/parallel-dashbord/@notifications/page.js
@@ -3,6 +3,9 @@ import React from 'react'
 
 export default async function NotificationPage() {
   await deLayFunc(3000)
+
+  throw new Error("error in notification")
+
   return (
     <div className='border text-2xl border-white flex justify-center items-center h-[500px] row-span-2'>
       <h1>Notification</h1>
---
+++ Next.js Class/module-8-advance-routing/app/parallel-dashbord/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div>
+      <h1 className='text-yellow-500 text-3xl'></h1>
+    </div>
+  )
+}
