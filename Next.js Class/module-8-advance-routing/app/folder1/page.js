16c278e0ae18e02c2e179181c3ad3d977ec77b2e
Shamiul40 <shamiul4040@gmail.com>
2026-01-31 22:48:17 +0600

    edit

--- Next.js Class/module-8-advance-routing/app/folder1/page.js
+++ Next.js Class/module-8-advance-routing/app/folder1/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Folder1() {
   return (
     <div className='min-h-44 flex flex-col '>
-      <h1>folder 1</h1>
+      <h1>folder 1 here</h1>
       <button><Link href="/folder1/folder2" className='text-blue-500'>go to folder 2</Link></button>
     </div>
   )
