fd0daafb6634d488b338bffbdb7bbd71ef39249b
Shamiul40 <shamiul4040@gmail.com>
2026-01-31 23:27:16 +0600

    add

--- Next.js Class/module-8-advance-routing/app/parallel-dashbord/@assignments/default.js
+++ Next.js Class/module-8-advance-routing/app/parallel-dashbord/@assignments/default.js
@@ -6,7 +6,7 @@ export default async function Default() {
 
   return (
     <div className='border text-2xl border-white flex justify-center items-center h-[250px]'>
-      <h1>Default Assignment here</h1>
+      <h1>Default Assignment</h1>
     </div>
   )
 }
