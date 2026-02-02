3bff17cbfa9c3393758e0e14360884a71083b6f8
Shamiul40 <shamiul4040@gmail.com>
2026-02-02 23:04:15 +0600

    dynamic route

--- Next.js Class/route-handler-8.5-get-post-delete/app/api/comments/[id]/route.js
+++ Next.js Class/route-handler-8.5-get-post-delete/app/api/comments/[id]/route.js
@@ -1,2 +1,9 @@
+import comments from "@/app/data/comments"
 
-export const PATCH 
+export const GET =async(request, {params}) =>{
+  const commentId = params.id
+
+  const comment = comments.find(comment=>comment.id === parseInt(commentId))
+
+  return Response.json(comment)
+}
