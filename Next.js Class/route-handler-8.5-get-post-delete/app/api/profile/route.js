b8ba9770d388b87407b4c47ec8f642643d3b0b2a
Shamiul40 <shamiul4040@gmail.com>
2026-02-03 11:44:32 +0600

    authorization

--- Next.js Class/route-handler-8.5-get-post-delete/app/api/profile/route.js
+++ Next.js Class/route-handler-8.5-get-post-delete/app/api/profile/route.js
@@ -1,5 +1,11 @@
+import { headers } from "next/headers";
+
 export async function GET(request){
     const requestHeaders = new Headers(request.headers);
+    // another way to get headers..this is default next.js function
+    const headersList = headers()
+
     console.log(requestHeaders.get("Authorization"));
+    console.log(headersList.get("Authorization"));
     return new Response("profile api")
 }
