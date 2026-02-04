5d8a20924bac0afd95fb889c440fcc6566cd5f04
Shamiul40 <shamiul4040@gmail.com>
2026-02-04 20:19:53 +0600

    change

--- Next.js Class/route-handler-8.5-get-post-delete/middleware.js
+++ Next.js Class/route-handler-8.5-get-post-delete/middleware.js
@@ -1,18 +1,35 @@
-import { NextResponse } from "next/server"
+// import { NextResponse } from "next/server"
 
-export function middleware(request){
-    console.log("hello")
-    
-   console.log(request.url)
+// export function middleware(request){
+//     console.log("hello")
+
+//    console.log(request.url)
 
 
-    return NextResponse.redirect(new URL("/", request.url))
+//     return NextResponse.redirect(new URL("/", request.url))
 
 
   
 
-}
+// }
 
-export const config ={
-    matcher : "/api",
+// export const config ={
+//     matcher : "/api",
+// }
+
+
+
+
+const defaultLocale = "en"
+let locales = ["en", "bn"]
+
+
+export function middleware(request) {
+    const pathName = request.nextUrl.pathName
+
+    const pathNameIsMissingLocale = locales.every(locale=> 
+        !pathName.startsWith(`/${locale}`) &&
+        !pathName.startsWith(`/${locale}/`)
+    
+    )
 }
