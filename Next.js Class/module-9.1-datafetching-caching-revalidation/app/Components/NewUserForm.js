8d5a49105697b49e4898f4e572e86dadec552a60
Shamiul40 <shamiul4040@gmail.com>
2026-02-13 10:41:10 +0600

    new user form create

---
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/Components/NewUserForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NewUserForm() {
+  return (
+    <div>
+       <div>
+            <input name='name'/>
+       </div>
+    </div>
+  )
+}
--- Next.js Class/module-9.1-datafetching-caching-revalidation/app/lib/getPost.js
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/lib/getPost.js
@@ -3,7 +3,7 @@ export const getPost=async()=>{
     const res = await fetch("https://api.chucknorris.io/jokes/random")
 
     if(!res.ok) {
-        throw new Error("response is not ok") 
+        throw new Error("response is not ok...") 
     }
 
     return await res.json()
