6f10755e4f87d44648c915ad4cb50c15c9058f1b
Shamiul40 <shamiul4040@gmail.com>
2026-02-16 21:08:25 +0600

    all posts map

--- Next.js Class/module-9.1-datafetching-caching-revalidation/app/lib/getPosts.js
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/lib/getPosts.js
@@ -2,6 +2,10 @@
 export const getPosts =async()=> {
     const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
 
-    
+    if(!posts.ok) {
+        throw new Error("post does not found")
+    }
+
+
     return posts.json();
 }
--- Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/page.js
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/page.js
@@ -3,11 +3,14 @@ import React from 'react'
 
 export default async function SequentialPage() {
 
-    const posts = getPosts
+    const posts = await getPosts();
+    console.log(posts)
 
   return (
-    <div>
-      SequentialPage
+    <div className='mt-10'>
+      {posts.length> 0 ? <ul>{posts.map(post=> (
+        <li key={post.id}>{post.id}. {post.title}</li>
+      ) )}</ul> : <div>data not found</div>}
     </div>
   )
 }
