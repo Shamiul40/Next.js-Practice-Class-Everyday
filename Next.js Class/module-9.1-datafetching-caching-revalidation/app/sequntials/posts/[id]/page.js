d2945c543d7e942accc9b79dcca7a8cf7d5d8d56
Shamiul40 <shamiul4040@gmail.com>
2026-02-16 21:25:56 +0600

    data fetch

--- Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/[id]/page.js
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/[id]/page.js
@@ -4,10 +4,10 @@ import React from 'react'
 export default async function PostPage({params : {id}}) {
 
     const post = await getPost(id)
-    console.log(post)
+   
   return (
-    <div>
-      <h1>get post</h1>
+    <div className='mt-10'>
+      <h1 className='border border-blue-300 px-2 py-1'>{post.title}</h1>
     </div>
   )
 }
