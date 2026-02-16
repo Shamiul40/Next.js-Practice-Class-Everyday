c83089fa4c25bb302a7a91e9bdaefc2506057d99
Shamiul40 <shamiul4040@gmail.com>
2026-02-16 21:12:13 +0600

    loading page

--- Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/page.js
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/sequntials/posts/page.js
@@ -1,16 +1,26 @@
-import { getPosts } from '@/app/lib/getPosts'
-import React from 'react'
+import { getPosts } from "@/app/lib/getPosts";
+import Link from "next/link";
+import React from "react";
 
 export default async function SequentialPage() {
-
-    const posts = await getPosts();
-    console.log(posts)
+  const posts = await getPosts();
+  console.log(posts);
 
   return (
-    <div className='mt-10'>
-      {posts.length> 0 ? <ul>{posts.map(post=> (
-        <li key={post.id}>{post.id}. {post.title}</li>
-      ) )}</ul> : <div>data not found</div>}
+    <div className="mt-10">
+      {posts.length > 0 ? (
+        <ul>
+          {posts.map((post) => (
+            <Link href={`sequntials/posts/${post.id}`} key={post.id}>
+            <li >
+              {post.id}. {post.title}
+            </li>
+            </Link>
+          ))}
+        </ul>
+      ) : (
+        <div>data not found</div>
+      )}
     </div>
-  )
+  );
 }
