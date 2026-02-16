eab7214e6f932821711ac7a9f0bb5d5654c4c8ba
Shamiul40 <shamiul4040@gmail.com>
2026-02-16 20:34:31 +0600

    Headers create

---
+++ Next.js Class/module-9.1-datafetching-caching-revalidation/app/Components/Headers.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Headers() {
+  return (
+    <div>
+      <h1>Patans and best practice</h1>
+      <div>
+        <div className="flex justify-center items-center">
+          <h1 className="border border-blue-600 px-8 py-4 bg-blue-300 text">Sequntial data fetching</h1>
+        </div>
+        <div className="">
+          <h1>Parallal data fetching</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
